4a655aeeb732a635f22d109c0b803ddee5c7f1fd
Максим Кузнецов <mvkuznetsov_3@edu.hse.ru>
2024-08-03 16:30:32 +0300

    Add guide view

---
+++ ios-app/iosApp/iosApp/Sources/Views/GuideView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Максим Кузнецов on 03.08.2024.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+            }
+            .navigationTitle("Гид")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        // TODO: add action.
+                    } label: {
+                        Image(systemName: "map")
+                            .foregroundStyle(.black)
+                    }
+
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .background{
+                Image("Main")
+                    .ignoresSafeArea(edges: .all)
+            }
+        }
+    }
+}
+
+#Preview {
+    GuideView()
+}
--- ios-app/iosApp/iosApp/iosApp.swift
+++ ios-app/iosApp/iosApp/iosApp.swift
@@ -15,7 +15,7 @@ struct iosApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GuideView()
         }
     }
 }
