00f99a7e629f3724fd281734c438ebf229946ef7
Максим Кузнецов <mvkuznetsov_3@edu.hse.ru>
2024-08-03 15:13:37 +0300

    Initial commit

---
+++ ios-app/iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Максим Кузнецов on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
