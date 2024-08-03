68c29088a6e7072b96574956ef6d389a9b5aaf45
Максим Кузнецов <mvkuznetsov_3@edu.hse.ru>
2024-08-03 15:13:37 +0300

    Initial commit

---
+++ ios-app/iosApp/iosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Максим Кузнецов on 03.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
