4d6d42757341f06f1f39c1cda7ece63d7fb6b68c
hzkotya <ladymary712@yandex.com>
2024-08-03 17:53:07 +0300

    upd

--- backend/pg_service_template/postgresql/schemas/db_1.sql
+++ backend/pg_service_template/postgresql/schemas/db_1.sql
@@ -5,7 +5,7 @@ DROP SCHEMA IF EXISTS guide CASCADE;
 CREATE SCHEMA IF NOT EXISTS guide;
 
 CREATE TABLE IF NOT EXISTS guide.places (
-    id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     lat DOUBLE PRECISION NOT NULL,
     lon DOUBLE PRECISION NOT NULL,
     name TEXT NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS guide.places (
 
 
 CREATE TABLE IF NOT EXISTS guide.selections (
-    id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     description TEXT NOT NULL,
     is_public BOOLEAN NOT NULL
@@ -29,32 +29,32 @@ CREATE TABLE IF NOT EXISTS guide.selections (
 
 
 CREATE TABLE IF NOT EXISTS guide.users (
-    id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS guide.places_selections (
-    place_id TEXT,
-    selection_id TEXT,
-    foreign key(place_id) REFERENCES guide.places(id),
-    foreign key(selection_id) REFERENCES guide.selections(id)
+    place_id UUID,
+    selection_id UUID,
+    -- foreign key(place_id) REFERENCES guide.places(id),
+    -- foreign key(selection_id) REFERENCES guide.selections(id)
 );
 
 CREATE TABLE IF NOT EXISTS guide.places_selections (
-    place_id TEXT,
-    selection_id TEXT,
-    foreign key(place_id) REFERENCES guide.places(id),
-    foreign key(selection_id) REFERENCES guide.selections(id)
+    place_id UUID,
+    selection_id UUID,
+    -- foreign key(place_id) REFERENCES guide.places(id),
+    -- foreign key(selection_id) REFERENCES guide.selections(id)
 );
 
 
 
 CREATE TABLE IF NOT EXISTS guide.visibility (
-    selection_id TEXT NOT NULL,
-    user_id TEXT NOT NULL,
-    foreign key(user_id) REFERENCES guide.users(id),
-    foreign key(selection_id) REFERENCES guide.selections(id)
+    selection_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    -- foreign key(user_id) REFERENCES guide.users(id),
+    -- foreign key(selection_id) REFERENCES guide.selections(id)
 );
 
 
