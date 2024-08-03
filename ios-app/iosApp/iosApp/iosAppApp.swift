07de0b56bbf2f4859cd54f0a9c92df44dde5ebf0
Максим Кузнецов <mvkuznetsov_3@edu.hse.ru>
2024-08-03 15:36:00 +0300

    Update iosAppApp.swift

--- ios-app/iosApp/iosApp/iosAppApp.swift
+++ ios-app/iosApp/iosApp/iosAppApp.swift
@@ -1,7 +1,18 @@
 import SwiftUI
+import YandexMapsMobile
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        YMKMapKit.setApiKey("d67cbfc8-2364-4890-a8c7-fe1268a02555")
+        YMKMapKit.sharedInstance()
+        return true
+    }
+}
 
 @main
 struct iosAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
