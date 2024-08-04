06f8bd0dc30f0f248a15180c90b14e7dfffccf94
Артур <bychkov.arthur@gmail.com>
2024-08-04 15:19:57 +0300

    add handler

---
+++ backend/pg_service_template/src/lib/error_description.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+enum class ErrorDescriprion {
+    kLowerLeftCornerNotSpecified,
+    kTopRightCornerNotSpecified,
+    kCornersNotSpecified,
+    kTokenNotSpecified,
+};
+
+extern const std::unordered_map<ErrorDescriprion, std::string> errorMapping;
--- backend/pg_service_template/src/main.cpp
+++ backend/pg_service_template/src/main.cpp
@@ -1,20 +1,25 @@
 #include <userver/clients/http/component.hpp>
+#include <userver/storages/postgres/component.hpp>
+#include <userver/components/component.hpp>
+#include <userver/clients/dns/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 
-#include "hello.hpp"
+#include "handlers/recommendations/recommendations.hpp"
 
 int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
                             .Append<userver::server::handlers::Ping>()
                             .Append<userver::components::TestsuiteSupport>()
                             .Append<userver::components::HttpClient>()
-                            .Append<userver::server::handlers::TestsControl>();
+                            .Append<userver::server::handlers::TestsControl>()
+                            .Append<userver::components::Postgres>("postgres-db-1")
+                            .Append<userver::clients::dns::Component>();
 
-  pg_service_template::AppendHello(component_list);
+  service::AppendRecommendations(component_list);
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
