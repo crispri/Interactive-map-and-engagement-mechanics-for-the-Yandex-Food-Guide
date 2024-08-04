8b2bb667e13013a35efb472b5d2292b5db2a0b76
Stanislav Leonchik <leonstas1@yandex.ru>
2024-08-04 19:12:26 +0300

    Add networking

---
+++ ios-app/iosApp/iosApp/Sources/Network/Configuration.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration.swift
+//  iosApp
+//
+//  Created by Stanislav Leonchik on 04.08.2024.
+//
+
+import Foundation
+
+struct Configuration {
+    static var baseURL: String {
+        "51.250.39.97:8080/guide/v1/"
+    }
+}
---
+++ ios-app/iosApp/iosApp/Sources/Network/Response/SnippetsResponse.swift
@@ -0,0 +1,12 @@
+//
+//  SnippetsResponse.swift
+//  iosApp
+//
+//  Created by Stanislav Leonchik on 04.08.2024.
+//
+
+import Foundation
+
+struct SnippetsResponse: ISnippetsResponse {
+    var items: [SnippetDTO]
+}
