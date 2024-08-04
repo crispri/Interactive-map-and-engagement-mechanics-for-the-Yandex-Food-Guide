4936d4068b17acd8559697fd567a042d1f72476c
Vladislav <chapkinvladya@yandex.ru>
2024-08-04 15:02:03 +0000

    fix conflict

--- backend/pg_service_template/postgresql/data/initial_data.sql
+++ backend/pg_service_template/postgresql/data/initial_data.sql
@@ -1,17 +1,22 @@
 
 INSERT INTO guide.selections(id, name, description, is_public) 
-VALUES('d0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Рестораны возсле Яндекса', 'Вкусно', 1);
+VALUES('d0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Рестораны возсле Яндекса', 'Вкусно', 1)
+ON CONFLICT DO NOTHING;
 
 INSERT INTO guide.selections(id, name, description, is_public) 
-VALUES('e0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Пиццерии возсле Яндекса', 'Быстро', 1);
+VALUES('e0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Пиццерии возсле Яндекса', 'Быстро', 1)
+ON CONFLICT DO NOTHING;
 
 
 
 INSERT INTO guide.places_selections(place_id, selection_id)
-VALUES('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'd0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11');
+VALUES('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'd0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11')
+ON CONFLICT DO NOTHING;
  
 INSERT INTO guide.places_selections(place_id, selection_id)
-VALUES('b0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'd0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11');
+VALUES('b0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'd0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11')
+ON CONFLICT DO NOTHING;
  
 INSERT INTO guide.places_selections(place_id, selection_id)
-VALUES('c0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'e0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11');
+VALUES('c0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'e0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11')
+ON CONFLICT DO NOTHING;
