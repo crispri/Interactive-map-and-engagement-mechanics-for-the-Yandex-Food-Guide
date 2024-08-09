77b2978fdb60efdfd1b3f9910465fbd9a58236dc
BychkovArthur <bychkovarthur@gmail.com>
2024-08-09 19:40:14 +0300

    all done

--- backend/pg_service_template/src/repository/IRestaurantRepository.hpp
+++ backend/pg_service_template/src/repository/IRestaurantRepository.hpp
@@ -5,16 +5,11 @@
 
 #include <models/restaurant.hpp>
 #include <models/restaurant_filter.hpp>
-#include <userver/components/component_list.hpp>
-#include <userver/components/component_base.hpp>
 
 
 namespace service {
 
-
-struct IRestaurantRepository : public userver::components::ComponentBase {
-
-
+struct IRestaurantRepository  {
 
     virtual std::vector<TRestaurant> GetAll() = 0;
     virtual std::optional<TRestaurant> GetById(const boost::uuids::uuid&) = 0;
