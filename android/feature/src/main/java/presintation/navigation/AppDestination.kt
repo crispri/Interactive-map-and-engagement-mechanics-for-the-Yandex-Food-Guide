8628b5d8324a71c38a0eba97b1c2a9f22fc81f7a
Megatroxx <mobilkaxxx@icloud.com>
2024-08-09 20:32:06 +0400

    main_screen

--- android/feature/src/main/java/presintation/navigation/AppDestination.kt
+++ android/feature/src/main/java/presintation/navigation/AppDestination.kt
@@ -3,4 +3,5 @@ package presintation.navigation
 object AppDestination {
     const val HOME_SCREEN = "home_screen"
     const val MAP_SCREEN = "map_screen"
+    const val RESTAURANT_SCREEN = "restaurant_screen"
 }
