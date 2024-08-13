d0e9cc650ba00887d984a22e3b6f56a3d855f490
Vova <vovabez04@gmail.com>
2024-08-13 17:52:56 +0300

    android-add-sexy-pin

---
+++ android/core/src/main/java/pins/NormalPinView.kt
@@ -0,0 +1,36 @@
+package pins
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.TextView
+import com.example.core.R
+
+class NormalPinView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val titleTextView: TextView
+    private val ratingTextView: TextView
+
+    init {
+        // Inflate the layout
+        inflate(context, R.layout.view_normal_pin, this)
+
+        // Get references to the views
+        titleTextView = findViewById(R.id.titleTextView)
+        ratingTextView = findViewById(R.id.ratingTextView)
+    }
+
+
+    fun setTitle(title: String) {
+        titleTextView.text = title
+    }
+
+    fun setRating(rating: String) {
+        ratingTextView.text = rating
+    }
+
+}
