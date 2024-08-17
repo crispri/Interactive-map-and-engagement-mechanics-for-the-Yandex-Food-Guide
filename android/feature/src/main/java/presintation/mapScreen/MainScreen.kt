808320016a01db6f16aa2edf9c0b25a731c064e3
Megatroxx <mobilkaxxx@icloud.com>
2024-08-17 22:47:28 +0400

    local changes last

--- android/feature/src/main/java/presintation/mapScreen/MainScreen.kt
+++ android/feature/src/main/java/presintation/mapScreen/MainScreen.kt
@@ -58,6 +58,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.pointer.pointerInput
