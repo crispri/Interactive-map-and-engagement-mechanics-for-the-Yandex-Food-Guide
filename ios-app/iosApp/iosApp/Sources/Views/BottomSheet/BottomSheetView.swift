68267f103729ccaa970da81fbd268f632b9ad471
HristinaBuyanova <hristina-a@mail.ru>
2024-08-18 12:23:59 +0300

    fix bug snippet

--- ios-app/iosApp/iosApp/Sources/Views/BottomSheet/BottomSheetView.swift
+++ ios-app/iosApp/iosApp/Sources/Views/BottomSheet/BottomSheetView.swift
@@ -16,6 +16,9 @@ struct BottomSheetView: View {
             VStack(spacing: 0) {
                 ForEach(viewModel.snippets, id: \.self) { snippet in
                     SnippetCell(restaurant: snippet)
+                        .sheet(isPresented: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is Presented@*/.constant(false)/*@END_MENU_TOKEN@*/, content: {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Sheet Content")/*@END_MENU_TOKEN@*/
+                        })
                 }
             }
             .padding(.bottom)
