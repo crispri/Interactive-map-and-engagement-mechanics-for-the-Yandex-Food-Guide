3761ab723ad08ea894cbc0cacc0464c1eae83915
Maxim <mvkuznetsov_3@edu.hse.ru>
2024-08-21 13:25:51 +0300

    Fix selections

--- ios-app/iosApp/iosApp/Sources/Views/Selections/SelectionView.swift
+++ ios-app/iosApp/iosApp/Sources/Views/Selections/SelectionView.swift
@@ -11,6 +11,7 @@ struct SelectionView: View {
     @State var title: String
     @State var desc: String
     @State var imageUrlString: String
+//    @State var type: SelectionType
     @Binding var selected: Bool
     var mainAction: (() -> Void)?
     var bookmarkAction: (() -> Void)?
@@ -20,72 +21,77 @@ struct SelectionView: View {
         Button {
             mainAction?()
         } label: {
-            HStack(alignment: .bottom) {
+            VStack(spacing: 0) {
                 if selected {
-                    Button {
-                        bookmarkAction?()
-                    } label: {
-                        Image(systemName: "bookmark")
-                            .tint(.white)
-                    }
-                    .frame(width: 40, height: 40, alignment: .bottomLeading)
-                }
-                Spacer()
-                
-                VStack {
-                    Text(title)
-                        .font(.system(size: 13))
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        .multilineTextAlignment(.center)
-                        .lineLimit(selected ? 1 : 2)
-                        .shadow(radius: 10)
-                    if selected {
-                        Text(desc)
+                    HStack {
+                        Button {
+                            bookmarkAction?()
+                        } label: {
+                            Image(systemName: "info.circle")
+                                .tint(.white)
+                        }
+                        Spacer()
+                        
+                        Text("9 мест")
+                            .font(.system(size: 11))
                             .foregroundStyle(.white)
                             .multilineTextAlignment(.center)
-                            .font(.system(size: 13))
-                            .shadow(radius: 10)
+                        
+                        Spacer()
+                        Button {
+                            bookmarkAction?()
+                        } label: {
+                            Image(systemName: "bookmark")
+                                .tint(.white)
+                        }
                     }
+                    .frame(height: 16)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
                 }
-                .padding(.vertical, 8)
-                .padding(.horizontal, selected ? 0 : 8)
+               
                 
-                Spacer()
-                if selected {
-                    Button {
-                        infoAction?()
-                    } label: {
-                        Image(systemName: "info.circle")
-                            .tint(.white)
-                    }
-                    .frame(width: 40, height: 40, alignment: .bottomTrailing)
-                }
+                Text(title)
+                    .font(.system(size: 13))
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 8)
             }
-            .background {
+            .shadow(color: .black, radius: 10)
+            .frame(width: selected ? 300 : 146, height: 60)
+            .background{
                 if let url = URL(string: imageUrlString) {
                     AsyncImage(url: url) { image in
                         image
                             .resizable()
                             .scaledToFill()
-                            .frame(
-                                width: selected ? UIScreen.main.bounds.width - 100 : 145.73,
-                                height: selected ? 80 : 60.05
-                            )
-                    } placeholder: { Color.gray.opacity(0.5) }
+                            .overlay {
+                                Color.gray.opacity(0.5)
+                            }
+                    } placeholder: { Color.gray }
+                } else {
+                    Color.gray
                 }
             }
-            .frame(
-                width: selected ? UIScreen.main.bounds.width - 100 : 145.73,
-                height: selected ? 80 : 60.05
-            )
-            .clipShape(RoundedRectangle(cornerRadius: 16.19))
-            .animation(.spring(duration: 0.2), value: selected)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
         }
+        .animation(.easeInOut, value: selected)
+    }
+    
+    enum SelectionType {
+        case common
+        case ultima
+        case openKitchen
     }
 }
 
 #Preview {
-    SelectionScrollView()
-        .environmentObject(SnippetViewModel())
+    SelectionView(
+        title: "Культура завтраков вне дома: какой-то фестиваль в Москве",
+        desc: "12 мест",
+        imageUrlString: "",
+//        type: .ultima,
+        selected: .constant(true)
+    )
 }
