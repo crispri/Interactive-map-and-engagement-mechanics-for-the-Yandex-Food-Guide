91f64def4797bb95db4b2bd10d89bcafe4866b54
Maxim <mvkuznetsov_3@edu.hse.ru>
2024-08-21 14:00:32 +0300

    Update SelectionView.swift

--- ios-app/iosApp/iosApp/Sources/Views/Selections/SelectionView.swift
+++ ios-app/iosApp/iosApp/Sources/Views/Selections/SelectionView.swift
@@ -21,78 +21,63 @@ struct SelectionView: View {
         Button {
             mainAction?()
         } label: {
-            HStack(alignment: .bottom) {
+            VStack(spacing: 0) {
                 if selected {
-                    Button {
-                        bookmarkAction?()
-                    } label: {
-                        Image(isSavedToCollection() ? "Bookmark.fill" : "Bookmark")
-                            .renderingMode(.template)
-                            .bold()
-                            .tint(.white)
-                            .padding([.bottom, .leading], 8)
-                    }
-                    .frame(width: 40, height: 40, alignment: .bottomLeading)
-                }
-                Spacer()
-                
-                VStack {
-                    Text(title)
-                        .font(.system(size: 13))
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        .shadow(color: .black, radius: 4)
-                        .multilineTextAlignment(.center)
-                        .lineLimit(selected ? 1 : 2)
-//                        .shadow(color: .black, radius: 4, x: 5, y: 5)
-                    if selected {
-                        Text(desc)
+                    HStack {
+                        Button {
+                            bookmarkAction?()
+                        } label: {
+                            Image(systemName: "info.circle")
+                                .tint(.white)
+                        }
+                        Spacer()
+                        
+                        Text("9 мест")
+                            .font(.system(size: 11))
                             .foregroundStyle(.white)
                             .multilineTextAlignment(.center)
-                            .lineLimit(2)
-                            .font(.system(size: 13))
-//                            .shadow(color: .black, radius: 4, x: 5, y: 5)
+                        
+                        Spacer()
+                        Button {
+                            bookmarkAction?()
+                        } label: {
+                            Image(systemName: "bookmark")
+                                .tint(.white)
+                        }
                     }
+                    .frame(height: 16)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
                 }
-                .padding(.vertical, 8)
-                .padding(.horizontal, selected ? 0 : 8)
                 
-                Spacer()
-                if selected {
-                    Button {
-                        infoAction?()
-                    } label: {
-                        Image(systemName: "info.circle")
-                            .tint(.white)
-                            .padding([.bottom, .trailing], 8)
-                    }
-                    .frame(width: 40, height: 40, alignment: .bottomTrailing)
+                
+                Text(title)
+                    .font(.system(size: 13))
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 8)
+            }
+            .shadow(color: .black, radius: 10)
+            .frame(width: selected ? 300 : 146, height: 60)
+            .background{
+                if let url = URL(string: imageUrlString) {
+                    AsyncImage(url: url) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .overlay {
+                                Color.black.opacity(0.5)
+                            }
+                    } placeholder: { Color.gray }
+                } else {
+                    Color.gray
                 }
             }
-            .background(
-                ZStack {
-                    if let url = URL(string: imageUrlString) {
-                        AsyncImage(url: url) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .frame(
-                                    width: selected ? UIScreen.main.bounds.width - 100 : 145.73,
-                                    height: selected ? 80 : 60.05
-                                )
-                        } placeholder: { Color.gray.opacity(0.5) }
-                    }
-                    LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.1), Color.black.opacity(0.8)]), startPoint: .top, endPoint: .bottom)
-                }
-                
-            )
-            .frame(
-                width: selected ? UIScreen.main.bounds.width - 100 : 145.73,
-                height: selected ? 80 : 60.05
-            )
-            .clipShape(RoundedRectangle(cornerRadius: 16.19))
-            .animation(.spring(duration: 0.2), value: selected)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
         }
+        .animation(.easeInOut, value: selected)
+        
     }
 }
 
